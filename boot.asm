d3a4bc362f9ad6ae5d796e2eb1400d295f516e12
Brandon Robinson <1802brad@gmail.com>
2026-02-18 18:19:54 -0500

    fix boot menu bug

--- boot.asm
+++ boot.asm
@@ -3,91 +3,45 @@ org 0x7c00
 
 mov si, 0
 
-; Print OS boot information
-
-print_os:
-    mov ah, 0x0e
-    mov al, [os + si]
+video_mode:
+    mov ah, 0x00
+    mov al, 0x03
     int 0x10
-    add si, 1
-    cmp byte [os + si], 0
-    jne print_os
 
-mov ah, 0x0e
-mov al, 0x0D
-int 0x10
+    lea si, [os]
+    call print_string
+    call new_line
 
-mov ah, 0x0e
-mov al, 0x0A
-int 0x10
+    lea si, [version]
+    call print_string
+    call new_line
 
+    lea si, [link]
+    call print_string
+    call new_line
 
-mov si, 0
+    lea si, [prompt]
+    call print_string
+    call new_line
 
-print_version:
-    mov ah, 0x0e
-    mov al, [version + si]
-    int 0x10
-    add si, 1
-    cmp byte [version + si], 0
-    jne print_version
+    lea si, [opt1]
+    call print_string
+    call new_line
 
-mov ah, 0x0e
-mov al, 0x0D
-int 0x10
+    lea si, [opt2]
+    call print_string
+    call new_line
 
-mov ah, 0x0e
-mov al, 0x0A
-int 0x10
-
-mov si, 0
-
-print_link:
-    mov ah, 0x0e
-    mov al, [link + si]
-    int 0x10
-    add si, 1
-    cmp byte [link + si], 0
-    jne print_link
-
-mov ah, 0x0e
-mov al, 0x0D
-int 0x10
-
-mov ah, 0x0e
-mov al, 0x0A
-int 0x10
-
-mov si, 0
-
-print_prompt:
-    mov ah, 0x0e
-    mov al, [prompt + si]
-    int 0x10
-    add si, 1
-    cmp byte [prompt + si], 0
-    jne print_prompt
-
-mov ah, 0x0e
-mov al, 0x0D
-int 0x10
-
-mov ah, 0x0e
-mov al, 0x0A
-int 0x10
-
-mov si, 0
-
-
+    lea si, [opt3]
+    call print_string
+    call new_line
 
 get_input:
-    
     mov ah, 0x00
     int 0x16
 
     cmp al, 0x0D
     je done_input
-
 
     cmp al, 0x73
     je match_s
@@ -95,51 +49,48 @@ get_input:
     jmp get_input
 
 match_s:
-
-    cmp byte [message_printed], 1
-    je get_input
-
-    mov si, 0
-    mov ah, 0x0e
-    mov al, '*'
-    int 0x10
-    mov al, 'S'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, 'c'
-    int 0x10
-    mov al, 'k'
-    int 0x10
-    mov al, 'O'
-    int 0x10
-    mov al, 'S'
-    int 0x10
-
-    mov byte [message_printed], 1
-
+    lea si, [message]
+    call print_string
     jmp get_input
-
 
 done_input:
     jmp $
 
+print_string:
+    push si
+    mov ah, 0x0e
 
+print_char:
+    mov al, [si]
+    cmp al, 0
+    je done_printing
+    int 0x10
+    inc si
+    jmp print_char
 
-; Boot print statements
+done_printing:
+    pop si
+    ret
 
-os:
-    db "Sock OS", 0
-version:
-    db "Version 1.00.0000 Sock OS", 0
-link:
-    db "https://github.com/brobinson1000/Sock-OS.git", 0
-prompt:
-    db "Boot Menu", 0 
-    input_buffer db 32 ; stores space for 32 characters
-    input_index db 0; track where use types
+new_line:
+    push si
+    mov ah, 0x0e
+    mov al, 0x0D
+    int 0x10
+    mov al, 0x0A
+    int 0x10
+    pop si
+    ret
 
-message_printed db 0
+os db "Sock OS", 0
+version db "Version 1.00.0000 Sock OS", 0
+link db "https://github.com/brobinson1000/Sock-OS.git", 0
+prompt db "Boot Menu", 0
+opt1 db "SockOS*", 0
+opt2 db "Advanced options for SockOS", 0
+opt3 db "Reboot System", 0
+message db "You entered S", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
+
