c1e48eb98d7ef79d80687a4bf459c77b9c5ebc37
Brandon <1802brad@gmail.com>
2026-08-23 16:02:52 -0500

    add caps lock abiliity

--- keyboard.c
+++ keyboard.c
@@ -19,7 +19,18 @@ static const char scancode_shift_to_ascii[] = {
     '*', 0, ' '
 };
 
+
+static const char scancode_caps_to_ascii[] = {
+    0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\n',
+    0, 'A', 'S', 'D', 'F', 'G', 'H', 'J',  'K', 'L', ';', '\'' , '`',
+    0, '\\' , 'Z', 'X', 'C', 'V' , 'B', 'N', 'M', ',', '.' , '/', 0,
+    '*', 0, ' '
+};
+
 static int shift_pressed = 0;
+static int caps_on = 0;
+
 
 void keyboard_callback(registers_t* regs) {
     (void)regs;
@@ -36,10 +47,21 @@ void keyboard_callback(registers_t* regs) {
         return;
     }
 
+    if (scancode == 0x3A ) {
+        caps_on ^= 1;
+        return;
+    }
+        
+
     if (scancode & 0x80) return;
 
     if (scancode < sizeof(scancode_to_ascii)) {
-        char c = shift_pressed ?  scancode_shift_to_ascii[scancode] : scancode_to_ascii[scancode];
+        char c;
+        if ( caps_on ) { 
+            c = scancode_caps_to_ascii[scancode]; 
+        } else {
+            c = shift_pressed ?  scancode_shift_to_ascii[scancode] : scancode_to_ascii[scancode];
+        }
         if (c) {
             char str[2] = {c, '\0'};
             kprint(str);
